28b2a30fd239863df34b27e3c59827bb47b1cb85
recurcive <recurcive@gmail.com>
2021-01-14 21:47:55 +0300

    Secont step: Added -f (choose format) argument

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -3,8 +3,9 @@
 import program from "commander";
 
 program
-    .version('0.0.1')
+    .version('1.0.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format [type]', 'output format')
     .parse(process.argv);
 
 if (!program.args.length) program.help();
