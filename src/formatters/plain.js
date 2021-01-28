72a2c74f593f8a3203cfa06210c56f8e86b449ff
recurcive <recurcive@gmail.com>
2021-01-28 09:12:08 +0300

    Added test for file unknown exception

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -6,19 +6,18 @@ const printPlainValue = (value) => (_.isObject(value) ? '[complex value]' : text
 
 const plain = (ast) => {
   const plainFormatter = (nested, parentKey = []) => nested.flatMap((node) => {
-    if (node.node === 'NESTED') {
-      return plainFormatter(node.children, [...parentKey, node.key]);
+    switch (node.node) {
+      case 'NESTED':
+        return plainFormatter(node.children, [...parentKey, node.key]);
+      case 'ADDED':
+        return `Property '${[...parentKey, node.key].join('.')}' was added with value: ${printPlainValue(node.value)}`;
+      case 'DELETED':
+        return `Property '${[...parentKey, node.key].join('.')}' was removed`;
+      case 'UPDATED':
+        return `Property '${[...parentKey, node.key].join('.')}' was updated. From ${printPlainValue(node.valueOld)} to ${printPlainValue(node.valueNew)}`;
+      default:
+        return [];
     }
-    if (node.node === 'ADDED') {
-      return `Property '${[...parentKey, node.key].join('.')}' was added with value: ${printPlainValue(node.value)}`;
-    }
-    if (node.node === 'DELETED') {
-      return `Property '${[...parentKey, node.key].join('.')}' was removed`;
-    }
-    if (node.node === 'UPDATED') {
-      return `Property '${[...parentKey, node.key].join('.')}' was updated. From ${printPlainValue(node.valueOld)} to ${printPlainValue(node.valueNew)}`;
-    }
-    return [];
   });
 
   return plainFormatter(ast).join('\n');
